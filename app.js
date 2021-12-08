c28276b72e7e030f40c3ff0729f799a76351caa5
George Pagonoudis <giorgospago23@gmail.com>
2021-12-08 21:39:27 +0200

    change port

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 80;
 
 
 app.use(cors());
